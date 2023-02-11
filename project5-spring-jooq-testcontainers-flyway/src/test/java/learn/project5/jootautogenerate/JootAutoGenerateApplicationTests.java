e4be1a7b9683bea22467995e2e01eb31278437f5
pavel bukhmatov <buhmatov@gmail.com>
2023-02-11 14:32:46 +0400

    add working example

--- project5-spring-jooq-testcontainers-flyway/src/test/java/learn/project5/jootautogenerate/JootAutoGenerateApplicationTests.java
+++ project5-spring-jooq-testcontainers-flyway/src/test/java/learn/project5/jootautogenerate/JootAutoGenerateApplicationTests.java
@@ -1,9 +1,15 @@
 package learn.project5.jootautogenerate;
 
+import com.example.jooq.Tables;
+import com.example.jooq.tables.Customers;
 import org.jooq.DSLContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static com.example.jooq.tables.Customers.CUSTOMERS;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class JootAutoGenerateApplicationTests {
@@ -11,9 +17,19 @@ class JootAutoGenerateApplicationTests {
     @Autowired
     private DSLContext dslContext;
 
-	@Test
-	void contextLoads() {
+    @Test
+    void contextLoads() {
 
-	}
+        dslContext.insertInto(Tables.CUSTOMERS)
+                .set(CUSTOMERS.ID, UUID.randomUUID())
+                .set(CUSTOMERS.FIRST_NAME, "foo")
+                .set(CUSTOMERS.LAST_NAME, "bar")
+                .execute();
+
+        dslContext.selectFrom(Tables.CUSTOMERS)
+                .fetch().forEach(it -> System.out.println("%s: %s %s".formatted(
+                        it.getId(), it.getFirstName(), it.getLastName()
+                )));
+    }
 
 }
