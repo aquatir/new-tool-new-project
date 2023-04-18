956bcbf6dbd27908404d228edbb4cf02cf7cee8f
pavel bukhmatov <buhmatov@gmail.com>
2023-04-18 09:42:03 +0400

    add basic code to save/load files

--- project7-go/http-web/main.go
+++ project7-go/http-web/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+// Page a wiki page
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+// save the file to local FS
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
 
 func main() {
 	fmt.Println("Hello")
+
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 }
