6b94528746c582dd10680ddef0e5867f3f0f4f76
pavel bukhmatov <buhmatov@gmail.com>
2023-04-22 20:49:00 +0400

    add go example using echo + SQLC

---
+++ project7-go/http-echo-db/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
+	"http-echo-db/dbdata"
+	"net/http"
+)
+
+type Payload struct {
+	Key string `json:"key"`
+}
+
+type Response struct {
+	Result bool `json:"result"`
+}
+
+type CustomContext struct {
+	echo.Context
+	db  *dbdata.Queries
+	ctx context.Context
+}
+
+func authorToString(author dbdata.Author) string {
+	return fmt.Sprintf("dbdata.Authhor{ID: %d, Name: %s, Bio: %s}", author.ID, author.Name, author.Bio.String)
+}
+
+func main() {
+
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=myuser password=mypassword dbname=mydatabase sslmode=disable")
+	if err != nil {
+		println(err.Error())
+		panic(1)
+	}
+
+	// Create a new SQLC client
+	sqlClient := dbdata.New(db)
+	ctx := context.Background()
+
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+
+	//curl localhost:1323
+	e.GET("/", hello)
+
+	// curl "localhost:1323/somepath/type=123&kek=wait"
+	e.GET("/:path", withPathAndQueryParams)
+
+	// curl -X POST -H "Content-Type: application/json" -d '{"key": "value"}' http://localhost:1323/post
+	e.POST("/post", postHandler)
+
+	// curl localhost:1323/db/create
+	e.GET("/db/create", handleWithDb(sqlClient, ctx, withDbInsertAndSelect))
+
+	// curl localhost:1323/db/all
+	e.GET("db/all", handleWithDb(sqlClient, ctx, dbGetAll))
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// expand default Context with DB access params
+func handleWithDb(client *dbdata.Queries, ctx context.Context, handlingFunction func(cc *CustomContext) error) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		cc := &CustomContext{c, client, ctx}
+		return handlingFunction(cc)
+	}
+}
+
+func withDbInsertAndSelect(c *CustomContext) error {
+	newAuthor, err := c.db.CreateAuthor(c.ctx, dbdata.CreateAuthorParams{
+		Name: "Ivan",
+		Bio: sql.NullString{
+			String: "first Ivan",
+		},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, fmt.Sprintf("Created author %s", authorToString(newAuthor)))
+}
+
+func dbGetAll(cc *CustomContext) error {
+	authors, err := cc.db.ListAuthors(cc.ctx)
+	if err != nil {
+		return err
+	}
+
+	var authorsStr string
+	for _, author := range authors {
+		authorsStr += fmt.Sprintf("%s, ", authorToString(author))
+	}
+
+	return cc.JSON(http.StatusOK, fmt.Sprintf("all authors: %d, values: %s", len(authors), authorsStr))
+}
+
+func postHandler(c echo.Context) error {
+
+	// read as payload
+	var requestPayload Payload
+	if err := c.Bind(&requestPayload); err != nil {
+		return err
+	}
+
+	fmt.Println("received request", requestPayload)
+
+	// Do something with the payload
+	response := &Response{
+		Result: true,
+	}
+	fmt.Println("returning response", response)
+	return c.JSON(http.StatusOK, response)
+
+}
+
+func withPathAndQueryParams(c echo.Context) error {
+	queryParam := c.QueryParams()
+	path := c.Param("path")
+
+	return c.String(http.StatusOK, "Hello "+path+" params: "+queryParam.Encode())
+}
+
+// Handler
+func hello(c echo.Context) error {
+	path := c.Request().URL.Path
+	return c.String(http.StatusOK, "Hello, World. Called on "+path)
+}
---
+++ project7-go/http-echo-db/readme.md
@@ -0,0 +1,16 @@
+
+1. 
+```bash
+docker-compose up
+```
+
+2. connect to DB
+
+3. Create schema using `db/schema.sql` file
+```postgresql
+CREATE TABLE authors (
+    id   BIGSERIAL PRIMARY KEY,
+    name text      NOT NULL,
+    bio  text
+);
+```
