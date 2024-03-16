bd6a4178676ed276dc7f47246aaecf7f744a7dc4
pavel bukhmatov <buhmatov@gmail.com>
2024-03-16 14:41:28 +0400

    Add ugly implementation

---
+++ project9-req-resp-capture/gateway/readme.md
@@ -0,0 +1,11 @@
+
+### GET
+```bash
+curl localhost:8080/get
+```
+
+
+### Post
+```bash
+curl -H "Content-Type: application/json" --data '{"username": "kek"}' -X POST localhost:8080/post
+```
--- project9-req-resp-capture/gateway/src/main/kotlin/learn/project9/demo/DemoApplication.kt
+++ project9-req-resp-capture/gateway/src/main/kotlin/learn/project9/demo/DemoApplication.kt
@@ -2,15 +2,17 @@ package learn.project9.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.filter.factory.rewrite.RewriteFunction
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.cloud.gateway.route.builder.filters
 import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
+import reactor.core.publisher.Mono
+import java.util.UUID
 
 @SpringBootApplication
 class DemoApplication {
-
     @Bean
     fun customRouteLocator(builder: RouteLocatorBuilder): RouteLocator {
         return builder.routes {
@@ -29,10 +31,84 @@ class DemoApplication {
                 }
                 uri("localhost:8081/kek")
             }
-            route("testing") {
+            route("httpbin-get") {
                 path("/get")
                 filters {
                     addRequestHeader("Hello", "World")
+                    modifyRequestBody(
+                        String::class.java, String::class.java
+                    ) { webExchange, originalBody ->
+                        val sb = StringBuilder()
+                        sb.append("Request ${webExchange.request.method} ${webExchange.request.uri}")
+                        sb.append(" Headers: ")
+                        webExchange.request.headers.forEach { key, value -> sb.append("$key: $value, ") }
+                        val returnBody = if (originalBody != null) {
+                            sb.append(" Body: $originalBody")
+                            Mono.just(originalBody)
+                        } else {
+                            Mono.empty()
+                        }
+                        println(sb)
+                        returnBody
+                    }
+                    modifyResponseBody(String::class.java, String::class.java,
+                        RewriteFunction { webExchange, originalBody ->
+                            val sb = StringBuilder()
+                            sb.append("Response ${webExchange.response.statusCode}")
+                            sb.append(" Headers: ")
+                            webExchange.request.headers.forEach { key, value -> sb.append("$key: $value, ") }
+                            val returnBody = if (originalBody != null) {
+                                sb.append(" Body: $originalBody")
+                                Mono.just(originalBody)
+                            } else {
+                                Mono.empty()
+                            }
+                            println(sb)
+                            returnBody
+                        }
+                    )
+                }
+                uri("http://httpbin.org:80")
+            }
+
+            route("httpbin-post") {
+                path("/post")
+                filters {
+                    addRequestHeader("Hello", "World")
+                    modifyRequestBody(
+                        String::class.java, String::class.java
+                    ) { webExchange, originalBody ->
+                        val sb = StringBuilder()
+                        val reqId = UUID.randomUUID()
+                        webExchange.attributes["req-id"] = reqId
+                        sb.append("RequestId '$reqId' Request ${webExchange.request.method} ${webExchange.request.uri}")
+                        sb.append(" Headers: ")
+                        webExchange.request.headers.forEach { key, value -> sb.append("$key: $value, ") }
+                        val returnBody = if (originalBody != null) {
+                            sb.append(" Body: $originalBody")
+                            Mono.just(originalBody)
+                        } else {
+                            Mono.empty()
+                        }
+                        println(sb)
+                        returnBody
+                    }
+                    modifyResponseBody(String::class.java, String::class.java,
+                        RewriteFunction { webExchange, originalBody ->
+                            val sb = StringBuilder()
+                            sb.append("RequestId '${webExchange.attributes["req-id"]}' Response ${webExchange.response.statusCode}")
+                            sb.append(" Headers: ")
+                            webExchange.request.headers.forEach { key, value -> sb.append("$key: $value, ") }
+                            val returnBody = if (originalBody != null) {
+                                sb.append(" Body: $originalBody")
+                                Mono.just(originalBody)
+                            } else {
+                                Mono.empty()
+                            }
+                            println(sb)
+                            returnBody
+                        }
+                    )
                 }
                 uri("http://httpbin.org:80")
             }
