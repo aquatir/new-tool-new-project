e8ed8d8bc252972fd004a169cffda5c86d71afa6
pavel bukhmatov <buhmatov@gmail.com>
2024-03-18 15:37:48 +0400

    Add logging filter

--- project9-req-resp-capture/gateway/src/main/kotlin/learn/project9/demo/DemoApplication.kt
+++ project9-req-resp-capture/gateway/src/main/kotlin/learn/project9/demo/DemoApplication.kt
@@ -1,19 +1,14 @@
 package learn.project9.demo
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.gateway.route.RouteLocator
-import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.cloud.gateway.route.builder.filters
 import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpMethod
-import org.springframework.web.server.ServerWebExchange
-import reactor.core.publisher.Mono
 import java.net.URI
-import java.util.UUID
 
 const val REQ_ID = "req-id"
 
@@ -32,16 +27,14 @@ data class ResponseData(
 @SpringBootApplication
 class DemoApplication {
     @Bean
-    fun customRouteLocator(builder: RouteLocatorBuilder, mappers: Mappers): RouteLocator {
-        val requestBodyMapper = mappers.requestBodyMapper()
-        val responseBodyMapper = mappers.responseBodyMapper()
+    fun customRouteLocator(builder: RouteLocatorBuilder, filters: Filters): RouteLocator {
+
         return builder.routes {
             route("httpbin") {
                 path("/httpbin/**")
                 filters {
                     rewritePath("/httpbin/(?<segment>.*)", "/\${segment}")
-                    modifyRequestBody(String::class.java, String::class.java, requestBodyMapper)
-                    modifyResponseBody(String::class.java, String::class.java, responseBodyMapper)
+                    filters.requestResponseLoggingFilter(this)
                 }
                 uri("http://httpbin.org")
             }
--- project9-req-resp-capture/gateway/src/main/kotlin/learn/project9/demo/Filters.kt
+++ project9-req-resp-capture/gateway/src/main/kotlin/learn/project9/demo/Filters.kt
@@ -1,15 +1,33 @@
 package learn.project9.demo
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
 import java.util.UUID
 
 @Component
-class Mappers(val objectMapper: ObjectMapper) {
+class Filters(val objectMapper: ObjectMapper) {
 
-    fun requestBodyMapper(): (ServerWebExchange, String) -> Mono<String> =
+    fun requestResponseLoggingFilter(myGatewayFilter: GatewayFilterSpec) {
+        myGatewayFilter.modifyRequestBody(String::class.java, String::class.java, requestBodyMapper)
+        myGatewayFilter.modifyResponseBody(String::class.java, String::class.java, responseBodyMapper)
+
+        // Remove all response headers starting with X-TG
+        myGatewayFilter.filter { exchange, chain ->
+            chain.filter(exchange)
+                .then(Mono.fromRunnable {
+                    exchange.response.headers.filter { it.key.startsWith("X-TG") }
+                        .forEach {
+                            exchange.response.headers.remove(it.key)
+                        }
+
+                });
+        }
+    }
+
+    private val requestBodyMapper: (ServerWebExchange, String) -> Mono<String> =
         { webExchange: ServerWebExchange, originalBody: String? ->
             val reqData = RequestData(
                 reqId = UUID.randomUUID().toString().also {
@@ -30,7 +48,7 @@ class Mappers(val objectMapper: ObjectMapper) {
             returnBody
         }
 
-    fun responseBodyMapper(): (ServerWebExchange, String) -> Mono<String> =
+    private val responseBodyMapper: (ServerWebExchange, String) -> Mono<String> =
         { webExchange: ServerWebExchange, originalBody: String? ->
             val respData = ResponseData(
                 reqId = webExchange.attributes[REQ_ID] as String?, headers = webExchange.response.headers
